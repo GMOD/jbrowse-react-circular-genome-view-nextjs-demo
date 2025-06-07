dc4146f164b0dcc4021882a8b79baed046342bea
Colin <colin.diesh@gmail.com>
2025-06-07 13:33:47 -0400

    Add nextjs config for false fs

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   basePath: '/demos/cgv-nextjs',
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      }
+    }
+    return config
+  },
 }
 
 module.exports = nextConfig
