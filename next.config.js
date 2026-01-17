29179ff02b1338f6d570ab979702eabe608ef4aa
Colin <colin.diesh@gmail.com>
2026-01-17 16:08:32 -0500

    CGV

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   basePath: '/demos/cgv-nextjs',
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-      }
-    }
-    return config
-  },
 }
 
 module.exports = nextConfig
